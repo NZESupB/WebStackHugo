f221a3e166d21d8a5ba35641e9cb978ed9dc083d
NZESupB <szboyxjw@gmail.com>
2022-11-28 09:06:42 +0800

    update scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-git pull && hugo
+git pull && rm -rf themes/WebStack-Hugo && git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo && hugo
--- themes/cloneWebStack-Hugo.sh
+++
@@ -1,1 +0,0 @@
-rm -r WebStack-Hugo && git clone https://github.com/shenweiyan/WebStack-Hugo.git
