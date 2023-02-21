235d5570a000c05e0740c2cf063d879d4089ddf8
NZESupB <szboyxjw@gmail.com>
2023-02-21 09:59:46 +0800

    update shell

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-git pull
 if [ -d "themes/WebStack-Hugo" ]; then
-  cd themes/WebStack-Hugo && git pull
+  cd themes/WebStack-Hugo && git pull && cd ../../ && git pull && hugo
 else
-  git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo
+  git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo && git pull && hugo
 fi
