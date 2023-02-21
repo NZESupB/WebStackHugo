0d588677b93809a0d7213a39a3d02b3d9f7ea523
NZESupB <szboyxjw@gmail.com>
2023-02-21 10:33:56 +0800

    update shell and website

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+apt install hugo -y
 if [ -d "themes/WebStack-Hugo" ]; then
   cd themes/WebStack-Hugo && git pull && cd ../../ && git pull && hugo
 else
   git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo && git pull && hugo
+fi
+
+if ! crontab -l | grep -q 'build.sh'; then
+    (crontab -l 2>/dev/null; echo "0 0 * * * cd /home/WebStackHugo && bash build.sh >> /home/wshugouautoupdate.log 2>&1") | crontab -
 fi
