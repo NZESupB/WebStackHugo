ccb4571b7c5c78e6ee6017851845ce0138463247
NZESupB <szboyxjw@gmail.com>
2023-02-21 09:44:28 +0800

    update all

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-git pull && rm -rf themes/WebStack-Hugo && git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo && hugo
+git pull
+if [ -d "themes/WebStack-Hugo" ]; then
+  cd themes/WebStack-Hugo && git pull
+else
+  git clone https://github.com/shenweiyan/WebStack-Hugo.git themes/WebStack-Hugo
+fi
